ae76e3a0aec5b8d665a36c9eb01dcd67e6d0d83f
ChuhanZhou <60944082+ChuhanZhou@users.noreply.github.com>
2020-10-31 01:33:20 +0100

    update

---
+++ Controllers/ModelManagerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using DNP_Assignment3_API.Data;
+using DNP_Assignment3_API.Models;
+using DNP_Assignment3_API.Models.List;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DNP_Assignment3_API.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class ModelManagerController : ControllerBase
+    {
+        private IModelManager modelManager;
+
+        public ModelManagerController()
+        {
+            modelManager = new ModelManager();
+        }
+
+        [Route("user")]
+        [HttpGet]
+        public async Task<ActionResult<UserList>> GetAllUser()
+        {
+            try
+            {
+                UserList userList = modelManager.GetAllUser();
+                return Ok(userList);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
---
+++ Data/ModelManager.cs
@@ -0,0 +1,158 @@
+﻿using DNP_Assignment3_API.Models;
+using DNP_Assignment3_API.Models.List;
+using DNP_Assignment3_API.Models.Unit;
+using DNP_Assignment3_API.Persistence;
+
+namespace DNP_Assignment3_API.Data
+{
+    public class ModelManager : IModelManager
+    {
+        private ModelPackage modelPackage;
+        private readonly string dataFileName;
+
+        public ModelManager()
+        {
+            modelPackage = new ModelPackage();
+            dataFileName = "DataFile.json";
+            ReadData();
+        }
+
+        private void ReadData()
+        {
+            DataFileContext.ReadData(dataFileName,modelPackage);
+        }
+
+        private void UpdateData()
+        {
+            DataFileContext.UpdateData(dataFileName,modelPackage);
+        }
+
+        public string AddUser(User newUser)
+        {
+            string result = modelPackage.UserList.AddUser(newUser);
+            UpdateData();
+            return result;
+        }
+
+        public bool Login(User user)
+        {
+            return modelPackage.UserList.CheckPassword(user);
+        }
+
+        public UserList GetAllUser()
+        {
+            return modelPackage.UserList.GetAllUser();
+        }
+
+        public string UpdatePassword(User oldUser,User newUser)
+        {
+            string result = modelPackage.UserList.UpdatePassword(oldUser,newUser);
+            UpdateData();
+            return result;
+        }
+
+        public void RemoveUser(User user)
+        {
+            modelPackage.UserList.RemoveUser(user);
+            UpdateData();
+        }
+
+        public string AddFamily(Family newFamily)
+        {
+            string result = modelPackage.FamilyList.AddFamily(newFamily);
+            UpdateData();
+            return result;
+        }
+
+        public FamilyList GetAllFamily()
+        {
+            return modelPackage.FamilyList.GetAllWithFamilyList();
+        }
+
+        public string UpdateFamily(Family oldFamily, Family newFamily)
+        {
+            string result = modelPackage.FamilyList.UpdateFamily(oldFamily, newFamily);
+            UpdateData();
+            return result;
+        }
+
+        public void RemoveFamily(Family family)
+        {
+            modelPackage.FamilyList.RemoveFamily(family);
+            UpdateData();
+        }
+
+        public string AddAdult(Adult newAdult)
+        {
+            if (modelPackage.ChildList.GetChildById(newAdult.Id)==null)
+            {
+                string result = modelPackage.AdultList.AddAdult(newAdult);
+                UpdateData();
+                return result;
+            }
+            return "This id is used.";
+        }
+
+        public AdultList GetAllAdult()
+        {
+            return modelPackage.AdultList.GetAllWithAdultList();
+        }
+
+        public string AddChild(Child newChild)
+        {
+            if (modelPackage.AdultList.GetAdultById(newChild.Id)==null)
+            {
+                string result = modelPackage.ChildList.AddChild(newChild);
+                UpdateData();
+                return result;
+            }
+            return "This id is used.";
+        }
+
+        public ChildList GetAllChild()
+        {
+            return modelPackage.ChildList.GetAllWithChildList();
+        }
+
+        public string UpdatePerson(Person newPerson)
+        {
+            string result = null;
+            if (modelPackage.AdultList.GetAdultById(newPerson.Id)!=null)
+            {
+                result = modelPackage.AdultList.UpdateAdult((Adult) newPerson);
+                if (result==null)
+                {
+                    foreach (var family in modelPackage.FamilyList.GetFamilyListByPerson(newPerson).families)
+                    {
+                        family.Adults.UpdateAdult((Adult) newPerson);
+                    }
+                }
+            }
+            else if (modelPackage.ChildList.GetChildById(newPerson.Id)!=null)
+            {
+                result = modelPackage.ChildList.UpdateChild((Child) newPerson);
+                if (result==null)
+                {
+                    foreach (var family in modelPackage.FamilyList.GetFamilyListByPerson(newPerson).families)
+                    {
+                        family.Children.UpdateChild((Child) newPerson);
+                    }
+                }
+            }
+            UpdateData();
+            return result;
+        }
+
+        public void RemovePerson(Person person)
+        {
+            modelPackage.AdultList.RemoveAdult(person);
+            modelPackage.ChildList.RemoveChild(person);
+            foreach (var family in modelPackage.FamilyList.families)
+            {
+                family.Adults.RemoveAdult(person);
+                family.Children.RemoveChild(person);
+            }
+            UpdateData();
+        }
+    }
+}
