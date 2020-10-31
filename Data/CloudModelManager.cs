971c2abd9ed8acbf1990aaaf516edf8e208c0e64
ChuhanZhou <60944082+ChuhanZhou@users.noreply.github.com>
2020-10-31 01:12:17 +0100

    update

--- Data/CloudModelManager.cs
+++ Data/CloudModelManager.cs
@@ -1,7 +1,83 @@
-﻿namespace DNP_Assignment3_API.Data
+﻿using DNP_Assignment3_API.Models.List;
+using DNP_Assignment3_API.Models.Unit;
+
+namespace DNP_Assignment3_API.Data
 {
-    public class CloudModelManager
+    public class CloudModelManager : IModelManager
     {
-        
+        public string AddUser(User newUser)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Login(User user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public UserList GetAllUser()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string UpdatePassword(User oldUser, User newUser)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveUser(User user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string AddFamily(Family newFamily)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public FamilyList GetAllFamily()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string UpdateFamily(Family oldFamily, Family newFamily)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveFamily(Family family)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string AddAdult(Adult newAdult)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public AdultList GetAllAdult()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string AddChild(Child newChild)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ChildList GetAllChild()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string UpdatePerson(Person newPerson)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemovePerson(Person person)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
