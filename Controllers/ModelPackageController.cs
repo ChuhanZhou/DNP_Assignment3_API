b1c48f3085cfa25deeb3de93dc6d91126615ca47
ChuhanZhou <60944082+ChuhanZhou@users.noreply.github.com>
2020-11-02 02:33:11 +0100

    update /api/all

---
+++ Controllers/ModelPackageController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using DNP_Assignment3_API.Models;
+using DNP_Assignment3_API.Persistence;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DNP_Assignment3_API.Controllers
+{
+    [ApiController]
+    [Route("api/all")]
+    public class ModelPackageController : ControllerBase
+    {
+        private ModelPackage modelPackage;
+
+        public ModelPackageController()
+        {
+            modelPackage = new ModelPackage();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ModelPackage>> GetAllData()
+        {
+            try
+            {
+                DataFileContext.ReadData("DataFile.json",modelPackage);
+                return Ok(JsonSerializer.Serialize(modelPackage,new JsonSerializerOptions {WriteIndented = true}));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
