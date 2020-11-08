2e1772095a20f371264e361832ee814f6c1e1645
ChuhanZhou <60944082+ChuhanZhou@users.noreply.github.com>
2020-11-09 00:58:30 +0100

    debug

--- Models/Unit/Interest.cs
+++ Models/Unit/Interest.cs
@@ -19,7 +19,7 @@ public class ValidInterest : ValidationAttribute {
     protected override ValidationResult IsValid(object value, ValidationContext validationContext)
     {
         List<string> valid = Interest.ValidInterest;
-        if (valid == null || valid.Contains(value.ToString().ToLower())) 
+        if (valid == null || valid.Contains(value.ToString())) 
         {
             return ValidationResult.Success;
         }
